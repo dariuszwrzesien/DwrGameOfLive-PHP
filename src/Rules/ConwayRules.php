77f1ec05a2ebd092553ff978bfb7010d67ed8653
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-04-17 21:34:25 +0200

    added test and overpopulation

--- src/Rules/ConwayRules.php
+++ src/Rules/ConwayRules.php
@@ -6,6 +6,16 @@ namespace Dwr\GameOfLive\Rules;
 use Dwr\GameOfLive\Entity\Cell;
 use Dwr\GameOfLive\Entity\Layout;
 
+/**
+ * Class ConwayRules
+ *
+ * 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation. [underPopulation]
+ * 2. Any live cell with more than three live neighbours dies, as if by overpopulation. [overpopulation]
+ * 3. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. [reproduction]
+ * 4. Any live cell with two or three live neighbours lives on to the next generation. [do not have to be implemented]
+ *
+ * @package Dwr\GameOfLive\Rules
+ */
 final class ConwayRules implements RuleInterface
 {
     /**
@@ -23,11 +33,10 @@ final class ConwayRules implements RuleInterface
 
         foreach ($layout->getCells() as $key => $cell) {
             $this->underPopulation($key, $cell);
+            $this->overPopulation($key, $cell);
         }
 
         return $this->layout;
-
-
     }
 
     /**
@@ -38,26 +47,22 @@ final class ConwayRules implements RuleInterface
      */
     private function underPopulation(int $cellIndex, Cell $cell)
     {
-
         if ($this->layout->countNeighbours($cell) < 2) {
             $this->layout->removeCell($cellIndex);
         }
     }
 
     /**
-     * Any live cell with two or three live neighbours lives on to the next generation.
-     */
-    private function stayLive()
-    {
-
-    }
-
-    /**
      * Any live cell with more than three live neighbours dies, as if by overpopulation.
+     *
+     * @param int $cellIndex
+     * @param Cell $cell
      */
-    private function overPopulation()
+    private function overPopulation(int $cellIndex, Cell $cell)
     {
-
+        if ($this->layout->countNeighbours($cell) > 3) {
+            $this->layout->removeCell($cellIndex);
+        }
     }
 
     /**
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -9,25 +9,89 @@ use PHPUnit\Framework\TestCase;
 
 class GameTest extends TestCase
 {
-    public function testIfOneRunOnDotTemplateReturnsEmptyLayoutBecauseOfUnderPopulation()
+    /**
+     *
+     * @dataProvider templateProvider
+     *
+     * @param string $jsonTemplate Template in json
+     * @param int    $runs         How many steps game should take
+     * @param int    $expected     Expected number of cells in layout
+     */
+    public function testIfRunOnGivenTemplateReturnsExpectedResult(string $jsonTemplate, int $runs, int $expected)
     {
-        $jsonTemplate = '{
-            "board": {
-                "length": 3,
-                "width": 3
-            },
-            "layout": {
-              "1": {
-                "lat": 2,
-                "lon": 2
-              }
-            }
-        }';
-
         $template = new Template($jsonTemplate);
         $game = new Game($template, new ConwayRules());
-        $game->run(1);
+        $game->run($runs);
 
-        $this->assertEquals(0, count($game->getLayout()->getCells()));
+        $this->assertEquals($expected, count($game->getLayout()->getCells()));
+    }
+
+    /**
+     * @return array
+     */
+    public function templateProvider()
+    {
+        return [
+            ['{
+                "board": {
+                    "length": 3,
+                    "width": 3
+                },
+                "layout": {
+                  "1": {
+                    "lat": 2,
+                    "lon": 2
+                  }
+                }
+            }', 1, 0],
+            ['{
+                "board": {
+                    "length": 5,
+                    "width": 5
+                },
+                "layout": {
+                  "1": {
+                    "lat": 2,
+                    "lon": 4
+                  },
+                  "2": {
+                    "lat": 2,
+                    "lon": 2
+                  },
+                  "3": {
+                    "lat": 4,
+                    "lon": 4
+                  },
+                  "4": {
+                    "lat": 4,
+                    "lon": 2
+                  }
+                }
+            }', 1, 0],
+            ['{
+                "board": {
+                    "length": 5,
+                    "width": 5
+                },
+                "layout": {
+                  "1": {
+                    "lat": 2,
+                    "lon": 3
+                  },
+                  "2": {
+                    "lat": 3,
+                    "lon": 4
+                  },
+                  "3": {
+                    "lat": 3,
+                    "lon": 2
+                  },
+                  "4": {
+                    "lat": 4,
+                    "lon": 3
+                  }
+                }
+            }', 1, 4]
+        ];
     }
 }
